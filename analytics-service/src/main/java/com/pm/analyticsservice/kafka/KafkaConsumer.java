1c4a93578dcaaf5faf48ecb23b326c99f0f33ede
harimz <harimzermeno@gmail.com>
2026-01-12 20:19:23 -0800

    Added api-gateway

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,31 @@
+package com.pm.analyticsservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+
+            // ... perform any business related to analytics
+
+            log.info("Received Patient Event: [PatientId={}, PatientFirstName={}, PatientLastName={}] " + "PatientEmail={}",
+                    patientEvent.getPatientId(),
+                    patientEvent.getFirstName(),
+                    patientEvent.getLastName(),
+                    patientEvent.getEmail());
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {}", e.getMessage());
+        }
+    }
+}
